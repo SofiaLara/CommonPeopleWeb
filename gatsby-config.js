c8104631b56c8ea3bf1dbc2cf80bc36099598d49
Peyret, Lara <sofiapeyret@gmail.com>
2022-08-06 00:48:50 +0100

    adding a plugin for resposive images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `Common People`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -178,12 +178,18 @@
 // Import React
 import * as React from "react";
 import Layout from "../components/layout";
+// Plugin for resposive images
+import { StaticImage } from "gatsby-plugin-image";
 
 // Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/lar.jpg"
+      />
     </Layout>
   );
 };
